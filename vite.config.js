525c97b7a33aff36262c1df70c1115a7a9103216
韩几 <hanji_0513@qq.com>
2025-04-20 03:52:44 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  // base: '/PersonalWebsite/',
-  base: './',
+  base: '/PersonalWebsite/',
+  // base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
